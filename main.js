799e35c8a8691ba9ca6098df2be8a95fd1398c09
JamesSewter <jamessewter@gmail.com>
2025-02-14 16:05:30 +0000

    add image, text, and link

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description:
+        'They go on your feet to keep them warm - often exchanged in large numbers at Christmas.',
+      image: './assets/images/socks_green.jpg',
+      url: "https://en.wikipedia.org/wiki/Sock"
+    };
+  },
+});
